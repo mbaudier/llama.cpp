3f1ea28ddc00a0f9d206dbab3d23d420318e8916
Mathieu Baudier <mbaudier@argeo.org>
2025-01-21 11:15:39 +0100

    Disable utilities not compatible with backend loading

--- debian/cmake/debian-llama.cpp.cmake
+++ debian/cmake/debian-llama.cpp.cmake
@@ -1,23 +1,22 @@
 cmake_minimum_required(VERSION 3.14) # for add_link_options and implicit target directories.
 
 # GGML dependencies
+# libggml-base as external library
 find_library(GGML_BASE_LOCATION ggml-base)
+message (STATUS "Found GGML base library: ${GGML_BASE_LOCATION}") 
+add_library(ggml-base SHARED IMPORTED GLOBAL)
+set_target_properties(ggml-base PROPERTIES IMPORTED_LOCATION ${GGML_BASE_LOCATION})
 
-# define GGML as target so that it is disabled in llama.cpp build
+# libggml as external library
+# defines GGML as target so that it is disabled in llama.cpp build
 find_library(GGML_LOCATION ggml)
 message (STATUS "Found GGML library: ${GGML_LOCATION}") 
 add_library(ggml SHARED IMPORTED GLOBAL)
 set_target_properties(ggml PROPERTIES IMPORTED_LOCATION ${GGML_LOCATION})
+# transitive dependency
+target_link_libraries(ggml INTERFACE ${GGML_BASE_LOCATION})
 
-# quite a few examples require direct reference to ggml-cpu
-# search for oldest one
-find_library(GGML_CPU_LOCATION ggml-cpu-sandybridge)
-find_library(GGML_RPC_LOCATION ggml-rpc)
-
-# make sure all libraries are available since we cannot refine per target
-link_libraries(${GGML_LOCATION} ${GGML_BASE_LOCATION} ${GGML_CPU_LOCATION} ${GGML_RPC_LOCATION})
-
-#add_compile_definitions(NDEBUG)
+add_compile_definitions(NDEBUG)
 
 install(DIRECTORY ${CMAKE_BINARY_DIR}/common/ DESTINATION lib/${CMAKE_LIBRARY_ARCHITECTURE}/llama.cpp/common FILES_MATCHING PATTERN "*.a" )
 install(DIRECTORY ${CMAKE_SOURCE_DIR}/common/ DESTINATION include/llama.cpp/common FILES_MATCHING PATTERN "*.h" )
